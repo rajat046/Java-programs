716192f01ba2b04774e52bac7d4f4a2b9c4f97eb
Rajat Talreja <37415458+rajat046@users.noreply.github.com>
2019-09-30 21:07:37 -0700

    Add files via upload

---
+++ Scan.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main
+{
+	public static void main(String[] args) {
+		int i;
+		float f;
+		
+		System.out.println("Enter values = ");
+		
+		Scanner sc = new Scanner(System.in);
+		i = sc.nextInt();
+		System.out.println("Integer = "+i);
+		
+		f = sc.nextFloat();
+		System.out.println("Float = "+f);
+	}
+}
