29135a59d4712ed386d2696e90969fd7e33dbb07
Rajat Talreja <37415458+rajat046@users.noreply.github.com>
2019-10-02 16:14:59 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,14 @@
+public class Pattern{
+    public static void main(String args[]){
+        int i, j, k;
+        for(i = 1; i < 10; i++){
+            for(k = 9; k > i; k--){
+                System.out.print("  ");
+            }
+            for(j = 1; j < i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
