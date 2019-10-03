d82ba6343e910e8b78d05bc27af65d93f951d58b
Rajat Talreja <37415458+rajat046@users.noreply.github.com>
2019-10-03 15:56:23 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Average{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum;
+        int average;
+        sum = a + b;
+        average = sum/2;
+        System.out.println("Average = "+average);
+    }
+}
