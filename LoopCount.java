f2c910d66751aaece6ebd3af22ff0de0db5bdb40
Rajat Talreja <37415458+rajat046@users.noreply.github.com>
2020-01-05 00:51:59 +0530

    Add files via upload

---
+++ LoopCount.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class LoopCount
+{
+    
+    public static void main(String args[])
+    {
+        System.out.print("Enter how many times you want to run the loop = ");
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for(int i = 1; i <= n; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
