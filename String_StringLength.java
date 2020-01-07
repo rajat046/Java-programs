69a9f77a844e4f39d2c31f3325ab9f54dd43c4e7
Rajat Talreja <37415458+rajat046@users.noreply.github.com>
2020-01-07 21:31:19 +0530

    Add files via upload

---
+++ String_StringLength.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class String_StringLength
+{
+    String s;
+    
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.print("Enter String = ");
+	    String s = sc.nextLine();
+	    int length = s.length();
+		System.out.println("The String you entered = "+s);
+		System.out.println("String Length = "+length);
+		
+	}
+}
