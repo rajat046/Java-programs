ae8140648776b34ab0d62e546a9c8386f14cac02
Rajat Talreja <37415458+rajat046@users.noreply.github.com>
2020-01-09 06:38:03 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ReverseString
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter string to be reversed = ");
+        String name = sc.nextLine();
+        int length = name.length();
+        String reverse = "";
+        for(int i = length-1; i >= 0; i--)
+        {
+            reverse = reverse + name.charAt(i);
+        }
+        System.out.print("Reverse of the string = "+reverse);
+        sc.close();
+    }
+}
