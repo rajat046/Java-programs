c9b3bd9ab4c408bca51f8ba5941f5eb444dbbe6d
Rajat Talreja <37415458+rajat046@users.noreply.github.com>
2020-01-09 06:35:45 +0530

    Add files via upload

---
+++ ReverseNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ReverseNumber
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number to be reversed = ");
+        int number = sc.nextInt();
+        int reverse = 0;
+        int remainder = 0;
+        while(number != 0)
+        {
+            remainder = number % 10;
+            // System.out.println("remainder = "+remainder);
+            reverse = reverse * 10 + remainder;
+            // System.out.println("reverse = "+reverse);
+            number = number / 10;
+            // System.out.println("number = "+number);
+            
+        }
+        System.out.println(reverse);
+        sc.close();
+    }
+}
