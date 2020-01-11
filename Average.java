ace63e98e75e64b73ba555892b810e3ca83a9eaa
Rajat Talreja <37415458+rajat046@users.noreply.github.com>
2020-01-11 16:01:28 +0530

    Update Average.java

--- Average.java
+++ Average.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 class Average{
-    public static void main(String args[]){
+    public static void main(String args[])
+    {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
