4ea73760f6cdce8102492c6ab6fed60093524ef2
Rajat Talreja <37415458+rajat046@users.noreply.github.com>
2020-01-11 16:03:19 +0530

    Update LoopCount.java

--- LoopCount.java
+++ LoopCount.java
@@ -2,17 +2,16 @@ import java.util.Scanner;
 
 class LoopCount
 {
-    
-    public static void main(String args[])
-    {
-        System.out.print("Enter how many times you want to run the loop = ");
-        int n;
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        sc.close();
-        for(int i = 1; i <= n; i++)
+        public static void main(String args[])
         {
-            System.out.println(i);
-        }
-    }
-}
+            System.out.print("Enter how many times you want to run the loop = ");
+            int n;
+            Scanner sc = new Scanner(System.in);
+            n = sc.nextInt();
+            sc.close();
+            for(int i = 1; i <= n; i++)
+            {
+                System.out.println(i);
+            }
+       }
+ }
