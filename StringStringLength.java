d7b64e6025e67d006483fc1745edf57df1f961d9
Rajat Talreja <37415458+rajat046@users.noreply.github.com>
2020-01-11 16:07:14 +0530

    Update StringStringLength.java

--- StringStringLength.java
+++ StringStringLength.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 
 public class StringStringLength
 {
-    String s;
-    
-	public static void main(String[] args) {
+    	String s;
+	public static void main(String[] args) 
+	{
 	    Scanner sc = new Scanner(System.in);
 	    System.out.print("Enter String = ");
 	    String s = sc.nextLine();
 	    int length = s.length();
-		System.out.println("The String you entered = "+s);
-		System.out.println("String Length = "+length);
-		
+	    System.out.println("The String you entered = "+s);
+	    System.out.println("String Length = "+length);
+	    sc.close();
 	}
 }
