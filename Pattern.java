cc874472708341469d55cbd5e271b1f9bd0343e0
Rajat Talreja <37415458+rajat046@users.noreply.github.com>
2020-01-11 16:04:14 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -1,11 +1,15 @@
 public class Pattern{
-    public static void main(String args[]){
+    public static void main(String args[])
+    {
         int i, j, k;
-        for(i = 1; i < 10; i++){
-            for(k = 9; k > i; k--){
+        for(i = 1; i < 10; i++)
+        {
+            for(k = 9; k > i; k--)
+            {
                 System.out.print("  ");
             }
-            for(j = 1; j < i; j++){
+            for(j = 1; j < i; j++)
+            {
                 System.out.print(j + " ");
             }
             System.out.println();
