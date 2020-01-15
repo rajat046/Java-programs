e14bf661618bc157fb6e331f7fc1d99c6bec7d85
Rajat Talreja <37415458+rajat046@users.noreply.github.com>
2020-01-16 02:51:08 +0530

    Add files via upload

---
+++ ReverseStarPattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many rows = ");
+        int rows = sc.nextInt();
+        for (int i = rows; i >= 0; i--)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+            sc.close();
+        }
+    }
+}
