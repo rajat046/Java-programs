62298a18a7c09784987d2fa69fecc1cc66a31405
Rajat Talreja <37415458+rajat046@users.noreply.github.com>
2020-01-22 14:29:39 +0530

    Update ReverseStarPattern.java

--- ReverseStarPattern.java
+++ ReverseStarPattern.java
@@ -7,6 +7,7 @@ public class ReverseStarPattern
         Scanner sc = new Scanner(System.in);
         System.out.print("How many rows = ");
         int rows = sc.nextInt();
+        sc.close();
         for (int i = rows; i >= 0; i--)
         {
             for (int j = 0; j <= i; j++)
@@ -14,7 +15,6 @@ public class ReverseStarPattern
                 System.out.print("*" + " ");
             }
             System.out.println();
-            sc.close();
         }
     }
 }
