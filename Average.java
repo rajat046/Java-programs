dfc2018692d25faf4de524c532edead96df5fdcb
Rajat Talreja <37415458+rajat046@users.noreply.github.com>
2020-01-22 14:24:56 +0530

    Update Average.java

--- Average.java
+++ Average.java
@@ -7,11 +7,12 @@ class Average
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
+        sc.close();
         int sum;
         int average;
         sum = a + b;
         average = sum/2;
-        sc.close();
+        
         System.out.println("Average = "+average);
     }
 }
