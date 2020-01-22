47f7d53bd52b7bfc62791bd719039253df3818fd
Rajat Talreja <37415458+rajat046@users.noreply.github.com>
2020-01-22 14:33:00 +0530

    Update Table.java

--- Table.java
+++ Table.java
@@ -7,11 +7,11 @@ class Tab
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter number");
 		a = sc.nextInt();
+		sc.close();
 		for(i = 1; i < 10001; i++)
 		{
 			t = a * i;
 			System.out.println(a+ " * " +i+ " = " +t);
 		}
-		sc.close();
 	}
 }
