31e2eff55d79e657f88c716850fed5690b5ddcc3
Rajat Talreja <37415458+rajat046@users.noreply.github.com>
2020-01-22 13:51:29 +0530

    Add files via upload

---
+++ MultiVarLoop.java
@@ -0,0 +1,9 @@
+public class MultiVarLoop
+{
+	public static void main(String[] args) {
+	    for(int i = 0, j = 10; i <= 10 && j >= 0; i++, j--)
+	    {
+	        System.out.println(" i = " +i+ " j = " +j);
+	    }
+	}
+}
