38f2233df37370420b40caa46c5d2bf19c39b122
Rajat Talreja <37415458+rajat046@users.noreply.github.com>
2020-01-22 22:28:37 +0530

    Add files via upload

---
+++ PattRev.java
@@ -0,0 +1,22 @@
+public class PattRev
+{
+  public static void main (String[]args)
+  {
+        for(int i = 10; i >= 1; i--)
+        {
+            for(int j = 0; j <= i; j++)
+            {
+                System.out.print("+ " + " ");
+            }
+            System.out.println();
+        }
+        for (int k = 0; k <= 10; k++)
+        {
+            for(int a = 0; a <= k; a++)
+            {
+                System.out.print("+ " + " ");
+            }
+            System.out.println();
+        }
+  }
+}
